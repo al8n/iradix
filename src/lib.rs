beaef584bb19a7de9c394c758bbff9ed10a10f3e
Al Liu <scygliu1@gmail.com>
2023-11-17 13:48:24 +0100

    upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,7 @@
-//! A template for creating Rust open-source repo on GitHub
+#![doc = include_str!("../README.md")]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![deny(missing_docs)]
 
-/// template
-pub fn it_works() -> usize {
-  4
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn test_works() {
-    assert_eq!(it_works(), 4);
-  }
-}
+extern crate alloc;
